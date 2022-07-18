2964b7d10e9b6d23b0cade8fd170696df9b7db7a
Marcos Berto <marcosberto66@gmail.com>
2022-07-18 01:49:55 -0300

    Adicionando ação aos botões

--- src/tic_tac_toe_game_java/Game.java
+++ src/tic_tac_toe_game_java/Game.java
@@ -10,9 +10,12 @@ import javax.swing.JFrame;
 public class Game extends JFrame implements ActionListener{
 	
 	private JButton buttons[] = new JButton[9];
+	private int player = 0;
+	private String players[] = {"X", "O"};
 	
 	public Game() {
-		setSize(600, 638);
+		//Definindo as características da JFrame/tela inicial.
+		setSize(605, 640);
 		setLocationRelativeTo(null);
 		setResizable(false);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -23,13 +26,12 @@ public class Game extends JFrame implements ActionListener{
 
 	private void init() {
 		
-		int width = (getWidth() / 3), height = ((getHeight() - 38) / 3);
-		System.out.println(width);
-		System.out.println(height);
-		int x = 0, y = 0;
+		int width = (getWidth() / 3) - 5, height = ((getHeight() - 40) / 3); //Tamanho dos botões
+		int x = 0, y = 0; //Posição inicial dos botões
 		int i = 0;
 		for (int l = 0; l < 3; l++) {
 			for (int c = 0; c < 3; c++) {
+				//Criando uma instância de JButton e definindo suas características
 				JButton button = new JButton();
 				button.setSize(width, height);
 				button.setLocation(x, y);
@@ -37,7 +39,7 @@ public class Game extends JFrame implements ActionListener{
 				button.addActionListener(this);
 				buttons[i] = button;
 				add(buttons[i]);
-				x += width;
+				x += width; 
 			}
 			x = 0;
 			y += height;
@@ -46,7 +48,15 @@ public class Game extends JFrame implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
+		JButton button = (JButton) e.getSource(); //Ao clicar em um JButton este método é executado
+		if(button.getText().isEmpty()) {
+			if(player == 0) {//Alternando entre os jogadores
+				button.setText(players[player]);
+				player = 1;
+			}else {
+				button.setText(players[player]);
+				player = 0;
+			}
+		}
 	}
 }
