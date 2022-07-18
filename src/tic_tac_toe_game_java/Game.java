0159589457ced7d2eefee7663279d70729a80692
Marcos Berto <marcosberto66@gmail.com>
2022-07-18 00:35:14 -0300

    Criando o cenário principal

---
+++ src/tic_tac_toe_game_java/Game.java
@@ -0,0 +1,20 @@
+package tic_tac_toe_game_java;
+
+import javax.swing.JFrame;
+
+public class Game extends JFrame{
+	
+	public Game() {
+		setSize(600, 600);
+		setLocationRelativeTo(null);
+		setUndecorated(true);
+		setResizable(false);
+		setLayout(null);
+		init();
+		setVisible(true);
+	}
+
+	private void init() {
+		
+	}
+}
---
+++ src/tic_tac_toe_game_java/Main.java
@@ -0,0 +1,8 @@
+package tic_tac_toe_game_java;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new Game();
+	}
+}
